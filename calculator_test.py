c1f69787ba97b94d423bdd769349e5747258a1b6
ahmed morgan <ahmed555morgan@gmail.com>
2022-03-03 08:06:49 -0800

    second try

--- calculator_test.py
+++ calculator_test.py
@@ -8,6 +8,15 @@ class Test(unittest.TestCase):
     def test_0_add(self):
         result = self.calculator.add(4,8)
         self.assertEqual(result,12)
+    def test_0_subtract(self):
+        result = self.calculator.subtract(8,4)
+        self.assertEqual(result,4)
+    def test_0_multiply(self):
+        result = self.calculator.multiply(4,4)
+        self.assertEqual(result,16)
+    def test_0_power(self):
+        result = self.calculator.power(2,3)
+        self.assertEqual(result,8)
 if __name__ == '__main__':
     # begin the unittest.main()
     unittest.main()
